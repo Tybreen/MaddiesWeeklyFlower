ed0e412a45e2b6234192626c799243d006a3025b
Tybreen <tyler.breen@icloud.com>
2023-05-25 16:59:15 -0400

    5/25/23

--- Main.js
+++ Main.js
@@ -6,8 +6,9 @@ Maddie's favorite flower: Chrysanthemum
 Date created: 10/18/22
 Given to Maddie: 4/21/23
 */
+
 /*Here*/
-var lastPublishedToGitHub = "Last Published to GitHub: 5/18/23";
+var lastPublishedToGitHub = "Last Published to GitHub: 5/25/23";
 
 // Vars:
 
@@ -31,17 +32,17 @@ var i = 0;
 / Year, Month, Day, Hour, Minute, Seconds /
 */
 /*Here*/
-var transitionDay = new Date(2023, 4, 21, 0, 0, 0);
-var nextTransitionDay = new Date(2023, 4, 28, 0, 0, 0);
+var transitionDay = new Date(2023, 4, 28, 0, 0, 0);
+var nextTransitionDay = new Date(2023, 5, 4, 0, 0, 0);
 
 var dayWeStartedDating = new Date(2022, 10, 22, 17, 30, 0);
 
 /*Here*/
-var quote = "When I spell flowers they take my breath away. You also, take MY breath away";
+var quote = "An apple a day, keeps the doctor away.  Maddie a day, keeps my heart beating all day.";
 
 // Horizontal : Vertical //
 /*Here*/
-var photoStyle1 = "Vertical";
+var photoStyle1 = "Horizontal";
 var photoStyle2 = "Horizontal";
 
 /*Here*/
@@ -52,8 +53,8 @@ function preload()
 {
 
     /*Here*/ 
-    image1 = loadImage("Flowers/Week 4.jpg");
-    image2 = loadImage("Flowers/Week 5.png");
+    image1 = loadImage("Flowers/Week 5.png");
+    image2 = loadImage("Flowers/Week 6.jpg");
 
 }
 
@@ -289,5 +290,6 @@ function dateDifference(start, end) {
         "Who is more Beautiful then these Flowers"
         "Who is the Thing I can't stop thinking about"
         "Roses are Red, Violets are Blue, and You're gorgeous too"
-        "When I spell flowers they take my breath away. You also, take MY breath away"
+        "When I spell flowers they take my breath away. You also, take MY breath away"  <-- SPELL!?! Oops.
+        "An apple a day, keeps the doctor away.  Maddie a day, keeps my heart beating all day."
   */
